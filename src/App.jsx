91faa7b61e6c5906f8256a2cda8649f9bdb60486
erlend <erlend.kodehode@gmail.com>
2024-05-08 18:41:09 +0200

    added moon icons for dark/light mode

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,18 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 import Api_call from "./components/api_call";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon as faMoonDark } from "@fortawesome/free-solid-svg-icons";
+import { faMoon as faMoonLight } from "@fortawesome/free-regular-svg-icons";
 
 export default function App() {
   return (
     <main className="App">
+      <header>
+        <h2>Where in the world</h2>
+        <FontAwesomeIcon icon={faMoonLight} className="light-mode" />
+        <FontAwesomeIcon icon={faMoonDark} className="dark-mode" />
+      </header>
       <Api_call />
     </main>
   );
