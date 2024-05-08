093b6c510c493ae64ed648960e25ad8203e64edf
erlend <erlend.kodehode@gmail.com>
2024-05-08 18:57:08 +0200

    added header with some styling

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,19 @@ import { faMoon as faMoonLight } from "@fortawesome/free-regular-svg-icons";
 
 export default function App() {
   return (
-    <main className="App">
-      <header>
+    <main className="app">
+      <header className="app_header">
         <h2>Where in the world</h2>
-        <FontAwesomeIcon icon={faMoonLight} className="light-mode" />
-        <FontAwesomeIcon icon={faMoonDark} className="dark-mode" />
+        <div className="app_header_dark-light-mode_container">
+          <div className="light-mode">
+            <FontAwesomeIcon icon={faMoonLight} />
+            <p>Dark mode</p>
+          </div>
+          <div className="dark-mode">
+            <FontAwesomeIcon icon={faMoonDark} />
+            <p>Light mode</p>
+          </div>
+        </div>
       </header>
       <Api_call />
     </main>
