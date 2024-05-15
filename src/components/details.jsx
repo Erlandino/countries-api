81c335dd3417997d1b4334d33f1a23c54c870c8d
erlend <erlend.kodehode@gmail.com>
2024-05-15 14:26:41 +0200

    used js find function to find country details from the api using the data from params

--- src/components/details.jsx
+++ src/components/details.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
 
 export default function Details({ countriesData }) {
   const { countryCode } = useParams();
 
+  const countryDetails = countriesData.find((filterData) => filterData.cca2 === countryCode);
+
   console.log(countryCode);
-  console.log(countriesData);
+  console.log(countryDetails);
   return (
     <section className="details">
       <p>testing some more</p>
