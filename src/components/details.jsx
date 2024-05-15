1f339492a671bba99022a9ffd7f6408348565305
erlend <erlend.kodehode@gmail.com>
2024-05-15 14:12:37 +0200

    made a way to get params with useparams

--- src/components/details.jsx
+++ src/components/details.jsx
@@ -1,4 +1,14 @@
-export default function App(a) {
-  console.log(a);
-  return <section className="details"></section>;
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+
+export default function Details({ countriesData }) {
+  const { countryCode } = useParams();
+
+  console.log(countryCode);
+  console.log(countriesData);
+  return (
+    <section className="details">
+      <p>testing some more</p>
+    </section>
+  );
 }
