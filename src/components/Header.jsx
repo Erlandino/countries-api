b1e8503b18799a2575ed21e7ed6cadc23c0f978c
erlend <erlend.kodehode@gmail.com>
2024-05-27 17:02:04 +0200

    added styling for new dark-light button in header, added button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,20 +3,36 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMoon as faMoonDark } from "@fortawesome/free-solid-svg-icons";
 import { faMoon as faMoonLight } from "@fortawesome/free-regular-svg-icons";
 
-export default function Header() {
+export default function Header({ toggleBackgroundColor, isBlack }) {
+  console.log(toggleBackgroundColor);
+  console.log(isBlack);
   return (
     <header className="app_header">
       <h2 className="app_header_title">Where in the world</h2>
       <div className="app_header_dark-light-mode_container">
-        <div className="light-mode">
-          <FontAwesomeIcon icon={faMoonLight} />
-          <p>Dark mode</p>
-        </div>
-        <div className="dark-mode">
-          <FontAwesomeIcon icon={faMoonDark} />
-          <p>Light mode</p>
-        </div>
+        {isBlack ? (
+          <button
+            onClick={toggleBackgroundColor}
+            className="app_header_dark-light-mode_container_button"
+          >
+            <FontAwesomeIcon icon={faMoonDark} />
+            <p>Light mode</p>
+          </button>
+        ) : (
+          <button
+            onClick={toggleBackgroundColor}
+            className="app_header_dark-light-mode_container_button"
+          >
+            <FontAwesomeIcon icon={faMoonLight} />
+            <p>Dark mode</p>
+          </button>
+        )}
       </div>
     </header>
   );
+}
+
+{
+  /*
+   */
 }
