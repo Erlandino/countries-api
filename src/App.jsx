f4bca857d3061af41569757ca497c74b19dd8354
erlend <erlend.kodehode@gmail.com>
2024-05-27 16:38:03 +0200

    added function and state for background color change for darkmode, also added some props for components

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,25 @@ import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import { Outlet } from "react-router-dom";
 import { countriesApiCall } from "./components/api";
 
-function Dashboard() {
+// elemnts to dispplay, header will always be ontop of the elements within outlet.
+function Dashboard({ toggleBackgroundColor, isBlack }) {
   return (
     <div className="app_container">
-      <Header />
+      <Header toggleBackgroundColor={(toggleBackgroundColor, isBlack)} />
       <Outlet />
     </div>
   );
 }
 
+// Outer component of project
 export default function App() {
   // useStates
   const [countriesData, setCountriesData] = useState();
+  const [isBlack, setIsBlack] = useState(false);
+
+  function toggleBackgroundColor() {
+    setIsBlack((prevState) => !prevState);
+  }
 
   // UseEffect for api call, to prevent infinite api due to setCountries in apiCall function
   useEffect(() => {
@@ -33,9 +40,19 @@ export default function App() {
   return (
     <main className="app">
       <Routes>
-        <Route path="/" element={<Dashboard />}>
-          <Route path="/" exact Component={() => <Countries countriesData={countriesData} />} />
-          <Route path="/:countryCode" Component={() => <Details countriesData={countriesData} />} />
+        <Route
+          path="/"
+          element={<Dashboard toggleBackgroundColor={toggleBackgroundColor} isBlack={isBlack} />}
+        >
+          <Route
+            path="/"
+            exact
+            Component={() => <Countries countriesData={countriesData} isBlack={isBlack} />}
+          />
+          <Route
+            path="/:countryCode"
+            Component={() => <Details countriesData={countriesData} isBlack={isBlack} />}
+          />
         </Route>
       </Routes>
     </main>
