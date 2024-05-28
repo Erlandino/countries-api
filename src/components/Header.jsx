a9c23a5a5ad3ff4321c89cee412d97816c3b4255
erlend <erlend.kodehode@gmail.com>
2024-05-28 16:39:32 +0200

    added some jsx and classes to header component for dark-light mode changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,32 +7,38 @@ export default function Header({ toggleBackgroundColor, isBlack }) {
   console.log(toggleBackgroundColor);
   console.log(isBlack);
   return (
-    <header className="app_header">
-      <h2 className="app_header_title">Where in the world</h2>
+    <header
+      className="app_header"
+      style={{ backgroundColor: isBlack ? "white" : "hsl(207, 26%, 17%)" }}
+    >
+      <h2 className="app_header_title" style={{ color: isBlack ? "black" : "white" }}>
+        Where in the world
+      </h2>
       <div className="app_header_dark-light-mode_container">
         {isBlack ? (
           <button
             onClick={toggleBackgroundColor}
-            className="app_header_dark-light-mode_container_button"
+            className="app_header_dark-light-mode_container_button app_header_dark-light-mode_container_light_button"
           >
-            <FontAwesomeIcon icon={faMoonDark} />
-            <p>Light mode</p>
+            <FontAwesomeIcon
+              icon={faMoonLight}
+              className="app_header_dark-light-mode_container_button_icon"
+            />
+            <p className="app_header_dark-light-mode_container_button_txt">Dark mode</p>
           </button>
         ) : (
           <button
             onClick={toggleBackgroundColor}
-            className="app_header_dark-light-mode_container_button"
+            className="app_header_dark-light-mode_container_button app_header_dark-light-mode_container_dark_button"
           >
-            <FontAwesomeIcon icon={faMoonLight} />
-            <p>Dark mode</p>
+            <FontAwesomeIcon
+              icon={faMoonDark}
+              className="app_header_dark-light-mode_container_button_icon"
+            />
+            <p className="app_header_dark-light-mode_container_button_txt">Light mode</p>
           </button>
         )}
       </div>
     </header>
   );
-}
-
-{
-  /*
-   */
 }
